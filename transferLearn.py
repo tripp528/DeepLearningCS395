13525e1f72b4fd5cd31994c70a0df15b71ef4ba1
Tripp Gordon <jgordo17@uvm.edu>
2020-01-28 14:53:57 -0500

    changed cache to be split in diff files

--- transferLearn.py
+++ transferLearn.py
@@ -15,7 +15,7 @@ from load_dataset import *
 flags = tf.app.flags
 flags.DEFINE_boolean("load_dataset",False,"Whether to save preprocessed dataset")
 flags.DEFINE_boolean("save_dataset",False,"Whether to save preprocessed dataset")
-flags.DEFINE_string("path_prep","Data/prep/prep.p","Number of samples")
+flags.DEFINE_string("path_prep","Data/prep/","Number of samples")
 
 flags.DEFINE_integer("num_samples",20000,"Number of samples")
 flags.DEFINE_integer("epoch1",1,"Epochs for first pass")
@@ -24,7 +24,7 @@ opt = flags.FLAGS
 print("num_samples:",opt.num_samples)
 
 
-def train(xval,yval):
+def train(xtrain,ytrain,xval,yval):
     # create the base pre-trained model
     base_model = InceptionV3(weights='imagenet', include_top=False)
 
@@ -100,9 +100,13 @@ if __name__ == '__main__':
         xtrain,xval,xtest, ytrain,yval,ytest = train_test_val_1hot(images_prep,labels)
 
         if opt.save_dataset == True:
-            pickle.dump((xtrain,xval,xtest, ytrain,yval,ytest), open(opt.path_prep, "wb"))
+            pickle.dump((xtrain,ytrain), open(opt.path_prep+"train.p", "wb"), protocol=4)
+            pickle.dump((xval, yval), open(opt.path_prep+"val.p", "wb"), protocol=4)
+            pickle.dump((xtest, ytest), open(opt.path_prep+"test.p", "wb"), protocol=4)
     else:
-        xtrain,xval,xtest, ytrain,yval,ytest = pickle.load(open(opt.path_prep,"rb"))
+        xtrain,ytrain = pickle.load(open(opt.path_prep + "train.p","rb"))
+        xval,yval = pickle.load(open(opt.path_prep + "val.p","rb"))
+        xtest,ytest = pickle.load(open(opt.path_prep + "test.p","rb"))
 
     #train model
-    train(xval,yval)
+    train(xtrain,ytrain,xval,yval)
